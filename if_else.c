2d0e89181c4a005c6b34cf8de9c4405c21fff1e8
Rithui <rithuikprakash12@gmail.com>
2022-11-20 11:39:13 +0530

    First commit

---
+++ if_else.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int age;
+    printf("Enter yout age\n");
+
+    scanf("%d",&age);
+    printf("you have entered %d as your age", 78age);
+    if (/* condition */)
+    {
+        /* code */
+    }
+    
+     return 0;
+}
